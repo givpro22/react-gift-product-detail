ae3ae0461f64d277bb764bc3697a1f3896641b32
youngseo <givpro22@daum.net>
2025-08-01 02:24:19 +0900

    chore: SenderSection, GiftMessageSection 메시지 입력 필드 유효성 검사 테스트 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
@@ -9,7 +9,11 @@ export default defineConfig({
       jsxImportSource: "@emotion/react",
     }),
   ],
-
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/tests/setupTests.ts",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
