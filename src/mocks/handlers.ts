f584e07f9b15ef71d4885da7047f060422dc246b
youngseo <givpro22@daum.net>
2025-08-01 03:02:50 +0900

    chore: MSW를 이용한 실시간 선물 랭킹 섹션 테스트 코드 추가

---
+++ src/mocks/handlers.ts
@@ -0,0 +1,26 @@
+import type { Product } from "@/api/products";
+import { http, HttpResponse } from "msw";
+
+const mockProduct: Product = {
+  id: 1,
+  name: "테스트 상품",
+  imageURL: "https://via.placeholder.com/150",
+  price: {
+    basicPrice: 10000,
+    sellingPrice: 8000,
+    discountRate: 20,
+  },
+  brandInfo: {
+    id: 1,
+    name: "테스트 브랜드",
+    imageURL: "https://via.placeholder.com/150",
+  },
+};
+
+export const handlers = [
+  http.get("/api/products/ranking", () => {
+    return HttpResponse.json({
+      data: [mockProduct],
+    });
+  }),
+];
